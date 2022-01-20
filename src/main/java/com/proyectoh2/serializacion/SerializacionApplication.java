d6446a956b52beab0cc7bb892155e7d304950973
camila-camano <63979137+camila-camano@users.noreply.github.com>
2022-01-20 13:20:57 -0300

    Add files via upload

---
+++ src/main/java/com/proyectoh2/serializacion/SerializacionApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoh2.serializacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerializacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerializacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoh2/serializacion/SerializacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoh2.serializacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerializacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
